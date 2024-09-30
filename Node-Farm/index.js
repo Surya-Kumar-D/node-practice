b93e18d95ab1a390f6aa08f50062788f674dc67d
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-09-30 19:59:51 +0530

    second commit

--- Node-Farm/index.js
+++ Node-Farm/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("../modules/replaceTemplate");
 
 ////////////////////////////////////////////////////////////////////////////////////
 //Blocking synchronous read and write
@@ -34,13 +35,52 @@ const url = require("url");
 
 //Server
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
   const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the overview page");
-  } else if (pathName === "/product") {
-    res.end("This is the product page");
-  } else {
+
+  //Overview Page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+  }
+
+  //Product Page
+  else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+  //API
+  else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  }
+  // Not Found
+  else {
     res.writeHead(404, {
       "content-type": "text/html",
       "my-own-header": "Hello World",

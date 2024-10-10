5fd33fcb72eacc2d59550950ee7816ed31420154
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-10-10 23:57:50 +0530

    Learning mongoose and mvc architeture

--- Natours/server.js
+++ Natours/server.js
@@ -1,6 +1,55 @@
+/* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = require('./app');
 

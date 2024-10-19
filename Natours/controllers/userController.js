31d93087c31084dabf8a1b8e273631fcc91e875c
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-10-19 20:47:11 +0530

    today i learned about authentication and it's importance. Also learned like jwt, hashing, bcryptjs!

--- Natours/controllers/userController.js
+++ Natours/controllers/userController.js
@@ -1,9 +1,21 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'Error',
-    message: 'This route is not yet defined',
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  //Send the response
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: users.length,
+    data: {
+      users: users,
+    },
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
